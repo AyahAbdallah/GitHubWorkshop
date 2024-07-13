36361a927e3b7f0eea27bff8f64fbd3766164c04
Ayah <ayah@gmail.com>
2024-07-13 11:04:38 +0300

    add all let function

---
+++ app.js
@@ -0,0 +1,8 @@
+let stars1 =document.getElementById('start');
+let moon2 =document.getElementById('moon');
+let mountains3 =document.getElementById('mountains3');
+let mountains4 =document.getElementById('mountains4');
+let river5 =document.getElementById('river5');
+let boat6  =document.getElementById('boat6');
+let mountains7 =document.getElementById('mountains7');
+let novel =document.querySelector('.novel');
