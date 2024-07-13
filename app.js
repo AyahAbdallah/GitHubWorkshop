c378e014245ca9b11a3bbb15821e3dac243cd594
Ayah <ayah@gmail.com>
2024-07-13 11:25:36 +0300

    add window.onscroll in js page

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ let river5 =document.getElementById('river5');
 let boat6  =document.getElementById('boat6');
 let mountains7 =document.getElementById('mountains7');
 let novel =document.querySelector('.novel');
+window.onscroll = function(){
+    let value = scrollY;
+    stars1.style.left = value +'px';
+    moon2.style.top = value * 3 + 'px';
+    mountains3.style.top = value * 2 + `px`;
+    mountains4.style.top = value * 1 + 'px';
+    river5.style.top = value + 'px';
+    boat6.style.top = value + 'px';
+    boat6.style.left = value*3 +'px';
+    novel.style.fontsize = value + 'px';
+    if (scrollY >= 127){
+        document.querySelector('.main').style.backgrouund = 'linear-gradient(#376281, #10001f)';
+    }
